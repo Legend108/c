f845f9525ae35accd85237950950157581a0e32e
Legend108 <80154701+Legend108@users.noreply.github.com>
2022-01-13 15:51:33 +0530

    Comments now make more sense

--- project/si.c
+++ project/si.c
@@ -1,13 +1,13 @@
-/* Project 1, 13/1/2022, Author :- Legend108 */
+/* Project 1, 13/1/2022, Legend108 */
 /* Simple interest */
 
+/* To be able to use printf() */
 #include <stdio.h>
 
 int main() {
 	/* Declaring the variables */
 	int prin, noy;
 	float rate, si;
-
 	/* Setting the principal, number of years, rate of interest */
 	prin = 1000;
 	noy = 3;
@@ -16,8 +16,8 @@ int main() {
 	/* Getting the result out */
 
 	si = prin * noy * rate / 100;
-	printf("%f\n", si);
+	printf("The simple interest is: %f\n", si);
 
-	/* Stop execution */
+	/* Return true */
 	return 0;
 }
