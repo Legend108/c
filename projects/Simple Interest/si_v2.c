2856cb6ce3df975f6b46181d96ba48b982775693
Legend108 <80154701+Legend108@users.noreply.github.com>
2022-01-13 16:15:51 +0530

    Create si_v2.c

---
+++ projects/Simple Interest/si_v2.c
@@ -0,0 +1,29 @@
+/* Project 1, 13/1/2022, Legend108 */
+/* Simple interest */
+
+/* To be able to use printf() */
+#include <stdio.h>
+
+int main() {
+	/* Declaring the variables */
+	int prin, noy;
+	float rate, si;
+
+	/* Setting the principal, number of years, rate of interest using user input */
+	printf("Enter the principal: ");
+	scanf("%d", &prin);
+
+	printf("Enter the number of years: ");
+	scanf("%d", &noy);
+
+	printf("Enter the rate: ");
+	scanf("%f", &rate);
+
+	/* Getting the result out */
+
+	si = prin * noy * rate / 100;
+	printf("The simple interest is: %f\n", si);
+	
+	/* Return true */
+	return 0;
+}
